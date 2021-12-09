f8359e12021afca04b58850e8d2dd584b8b09651
GabrielBolivar <gabrielbolivar86@gmail.com>
2021-12-09 10:18:30 -0400

    12/09/2021

---
+++ src/components/Page/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+.colored:nth-child(1) {
+background: Yellow;
+}
+.colored:nth-child(2) {
+background: #445577;
+}
+.colored:nth-child(3) {
+background: #AA3456;
+}
+.colored:nth-child(4) {
+background: #445480;
+}
+.colored:nth-child(5) {
+background: #f1faee;
+}
+.colored:nth-child(6) {
+background: #aa3697;
+}
+.colored:nth-child(7) {
+background: #77e68c;
+}
+.colored:nth-child(8) {
+background: black;
+}
+`;
